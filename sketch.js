4c14fe64754febe6e0d08993096d7e2c4452b930
Sakshi887220 <70653660+Sakshi887220@users.noreply.github.com>
2020-12-06 15:14:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,114 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world; 
+var bottomBoundary, topBoundary; 
+var leftBoundary, rightBoundary;
+var ground; 
+//var division1, division2, division3, division4, division5, division6, division7, division8;
+
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+
+function setup() {
+  createCanvas(600,800);
+  createSprite(400, 200, 50, 50);
+  engine = Engine.create(); 
+  world = engine.world;
+
+  //Make the Boundaries 
+
+  bottomBoundary = new Boundary(400,790,800,20);
+  topBoundary = new Boundary(400, 10, 800, 20); 
+  leftBoundary = new Boundary(10, 400,20,800);
+  rightBoundary = new Boundary(590,400,20,800);
+ ground = new Ground(300,780,560,12);
+  /*division1 = new Division(20, 600);
+  division2 = new Division(100,600);
+  division3 = new Division(180,600);
+  division4 = new Division(260,600);
+  division5 = new Division(340,600);
+  division6 = new Division(420,600);
+  division7 = new Division(500,600);
+  division8 = new Division(580,600);*/
+  
+  for (var k = 0; k <=width; k = k + 80) {
+    divisions.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+
+   for (var j = 75; j <=width; j=j+50) 
+   {
+   
+      plinkos.push(new Plinko(j,75));
+   }
+
+   for (var j = 50; j <=width-10; j=j+50) 
+   {
+   
+      plinkos.push(new Plinko(j,175));
+   }
+
+    for (var j = 75; j <=width; j=j+50) 
+   {
+   
+      plinkos.push(new Plinko(j,275));
+   }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+   {
+   
+      plinkos.push(new Plinko(j,375));
+   }
+
+   
+
+   
+
+
+
+}
+
+function draw() {
+  background("black");  
+  Engine.update(engine);
+  bottomBoundary.display();
+  topBoundary.display();
+  leftBoundary.display();
+  rightBoundary.display();
+  ground.display();
+  //division1.display();
+  //division2.display();
+  //division3.display();
+  //division4.display();
+ // division5.display();
+  //division6.display();
+  //division7.display();
+  //division8.display();
+
+  for (var i = 0; i < plinkos.length; i++) {
+     
+    plinkos[i].display();
+    
+  }
+
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+    
+  }
+
+  for (var j = 0; j < particles.length; j++) {
+   
+    particles[j].display();
+  }
+  for (var k = 0; k < divisions.length; k++) {
+    
+    divisions[k].display();
+  }
+
+
+}
